165d7eaf4f175092cdf03903657225983fd6a770
Cristhyan Kohlhase <cristhyan.ko@outlook.com>
2022-06-05 02:39:13 -0400

    Install dependencies and configure eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+	reactStrictMode: true,
+	env: {
+		API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/v1/",
+	},
 }
-
-module.exports = nextConfig
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,3 @@
 export default function Inicio() {
-  return (
-    <div>
-      Hello !
-    </div>
-  )
+	return <div>Hello !</div>
 }
